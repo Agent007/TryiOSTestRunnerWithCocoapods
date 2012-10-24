d78b469a272425bce0ed3f963d2b4dff8def8490
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-10-24 11:58:37 +1100

    Added -workspace option to xcodebuild calls

--- build_and_run_unit_tests.sh
+++ build_and_run_unit_tests.sh
@@ -30,12 +30,12 @@ XCODE_TEST_LDFLAGS="${LINK_TO_XCODE_TEST_LIB}"
 osascript -e 'tell app "iPhone Simulator" to quit'
 
 # Build the unit tests bundle, so it can be fed into waxsim
-xcodebuild -sdk iphonesimulator -scheme ${UNIT_TEST_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}"
 echo "========================="
 echo "Building unit test bundle"
 echo "========================="
 echo "xcodebuild -sdk iphonesimulator -scheme ${UNIT_TEST_TARGET} build CONFIGURATION_BUILD_DIR=\"${OUTPUT_DIR}\""
 echo "========================="
+xcodebuild -sdk iphonesimulator -workspace TryiOS6TestRunnerWithCocoapods.xcworkspace -scheme ${UNIT_TEST_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}"
 if [[ $? != 0 ]]; then
   echo "Failed to build unit tests!"
   exit $?
@@ -47,6 +47,7 @@ echo "Building app with xcodetest"
 echo "==========================="
 echo "xcodebuild -sdk iphonesimulator -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR=\"${OUTPUT_DIR}\" XCODE_TEST_LDFLAGS=\"${XCODE_TEST_LDFLAGS}\""
 echo "==========================="
+xcodebuild -sdk iphonesimulator -workspace TryiOS6TestRunnerWithCocoapods.xcworkspace -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}" XCODE_TEST_LDFLAGS="${XCODE_TEST_LDFLAGS}"
 if [[ $? != 0 ]]; then
   echo "Failed to build app!"
   exit $?
