ef25cf462317964fc5d66c402a6a8160b75b91b3
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-10-24 11:57:31 +1100

    Added some debugging prints into the script

--- build_and_run_unit_tests.sh
+++ build_and_run_unit_tests.sh
@@ -31,14 +31,22 @@ osascript -e 'tell app "iPhone Simulator" to quit'
 
 # Build the unit tests bundle, so it can be fed into waxsim
 xcodebuild -sdk iphonesimulator -scheme ${UNIT_TEST_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}"
+echo "========================="
+echo "Building unit test bundle"
+echo "========================="
+echo "xcodebuild -sdk iphonesimulator -scheme ${UNIT_TEST_TARGET} build CONFIGURATION_BUILD_DIR=\"${OUTPUT_DIR}\""
+echo "========================="
 if [[ $? != 0 ]]; then
   echo "Failed to build unit tests!"
   exit $?
 fi
 
 # Build the main app, with libXcodeTest.a linked in
+echo "==========================="
+echo "Building app with xcodetest"
+echo "==========================="
 echo "xcodebuild -sdk iphonesimulator -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR=\"${OUTPUT_DIR}\" XCODE_TEST_LDFLAGS=\"${XCODE_TEST_LDFLAGS}\""
-xcodebuild -sdk iphonesimulator -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}" XCODE_TEST_LDFLAGS="${XCODE_TEST_LDFLAGS}"
+echo "==========================="
 if [[ $? != 0 ]]; then
   echo "Failed to build app!"
   exit $?
